c134c24948ce0b1f10def67217b4274359332d77
Arfian29 <rofioy21@gmail.com>
2024-11-14 13:00:48 +0700

    Add Design Web

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Smoth Scroll
+document.addEventListener('DOMContentLoaded', function () {
+  // Ambil semua link dalam navbar, termasuk yang di dalam dropdown
+  const linksEl = document.querySelectorAll(
+    '.navbar a, .dropdown-content a, .btn-peta'
+  );
+
+  linksEl.forEach((link) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        // Hitung posisi scroll dengan mempertimbangkan tinggi navbar
+        const navbar = document.querySelector('.navbar');
+        const navbarHeight = navbar.offsetHeight;
+        const targetPosition =
+          targetElement.getBoundingClientRect().top +
+          window.pageYOffset -
+          navbarHeight;
+
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth',
+        });
+
+        // Jika menggunakan dropdown mobile, tutup dropdown setelah klik
+        const dropdown = document.querySelector('.dropdown');
+        if (dropdown) {
+          dropdown.classList.remove('open'); // Sesuaikan dengan class yang Anda gunakan untuk membuka dropdown
+        }
+      }
+    });
+  });
+});
